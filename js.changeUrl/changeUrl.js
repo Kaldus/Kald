31300331ac77d1ede99fd82ec75c89cd5f62f882
Kewsi Alleyne <info@kaldcraft.com>
2013-09-18 15:48:50 -0400

    Code correction.

--- js.changeUrl/changeUrl.js
+++ js.changeUrl/changeUrl.js
@@ -7,9 +7,10 @@ function toPage() { // start the function called toPage
     if (checkCat !== null) { // if the check for cat2 is not null (which means the current URL contains cat2) ...
         var fixTo = whereTo.replace('index.htm', '').replace('/cat2/', '/cat/'); // find and replace index file with null, and replace 'cat2' dir with just 'cat' dir
     } else fixTo = whereTo.replace('index.htm', ''); // if not (else), simply get rid of index filename if it is found, no need to worry about 'cat2' now
+    var length = fixTo.length;
     var findLast = fixTo.charAt(length - 1); // check the last character of the URL for forward slash '/'
     if (findLast != '/') { // if the last character of findLast is not a fwd slash
-        whereNow = fixTo + selected + '/index.htm'; // take the value of the adjusted fixTo URL and add the value pulled from the dropdown
-    } else whereNow = fixTo + '/' + selected + '/index.htm'; // else, if we found a slash at the end, add one in between the base URL and the value pulled from the dropdown value
+        whereNow = fixTo + '/' + selected + '/index.htm'; // take the value of the adjusted fixTo URL, add a slash & add the value pulled from the dropdown
+    } else whereNow = fixTo + selected + '/index.htm';  // else, if we found a slash at the end of the URLsimply add the value from dropdown
     document.location = whereNow; // change the browser location to the value of whereNow
 } //close this function
